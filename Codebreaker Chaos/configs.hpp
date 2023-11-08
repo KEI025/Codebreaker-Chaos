39bc549aab9df888f80f26dd3e633818758be077
KEI025 <kei.bash0731@gmail.com>
2023-11-09 00:11:01 +0900

    ゲーム難易度のリストを追加

--- Codebreaker Chaos/configs.hpp
+++ Codebreaker Chaos/configs.hpp
@@ -2,5 +2,6 @@
 #include "common.hpp"
 
 const Size kWindowSize{ 1000,750 };
+const Array<String> kGameModes { U"ESAY",U"NORMAL",U"HARD"};
 const String kGameTitle = U"CodeBreaker Chaos";
 const String kGameTitleCautionMsg = U"- 警告 -\n実際にコンピューターウイルスを作成することは\n法律で禁止されています。\n絶対にしないでください。";
